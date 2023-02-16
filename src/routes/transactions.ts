2f9beaf62888edd1b8e4070a732be973499f44ea
Emanuel Massafera <emanuel301@live.com>
2023-02-16 08:50:09 -0300

    feat: add route to list a transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -12,6 +12,17 @@ export async function transactionsRoutes(app: FastifyInstance) {
     })
   })
 
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return reply.status(200).send({ transaction })
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
