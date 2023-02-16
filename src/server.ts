6f8a3525c010e7a84a30ae2efd36aa6e7061a3a5
Emanuel Massafera <emanuel301@live.com>
2023-02-16 08:42:26 -0300

    feat: add route to create a transaction

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello Node.js!'
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
